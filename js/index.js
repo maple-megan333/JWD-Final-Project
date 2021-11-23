a9885cc5f64ebf6be62992b32f780b3d282aa8f3
Megan Maple <91995689+maple-megan333@users.noreply.github.com>
2021-11-23 22:14:25 +0000

    Completed steps 6 and 7 with no bugs. On to debut step 8.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
-//imports
+////imports
 //import { TaskManager } from "./taskManager";
+
+//taskManager.load();
+
 const taskManager = new TaskManager();
 
 const newTaskform = document.querySelector('#newTask');
 
-newTaskform.addEventListener('submit', (event) => {
 
+
+
+newTaskform.addEventListener('submit', (event) => {
+  
   event.preventDefault();
+
+  //input feild variable conversions
 
   const newName = document.querySelector('#newName');
   const newDescription = document.querySelector('#newDescription');
@@ -18,9 +26,19 @@ newTaskform.addEventListener('submit', (event) => {
   const assignedTo = newAssignedTo.value;
   const dueDate = newDueDate.value;
 
+  //adding a task
+
+  /*if (name === '') {
+    alert('This form was not filled out correctly please try again.')
+  };*/
+
   taskManager.addTask(name, description, assignedTo, dueDate);
- 
+
+  // rendering the task
+
   taskManager.render();
+
+  //clearing the form
 
   newName.value = '';
   newDescription.value = '';
@@ -34,4 +52,46 @@ newTaskform.addEventListener('submit', (event) => {
 });
 
 
+
+
+
+// creating change status functionality. 
+
+// creates a variable contaning the task cards areas
+const toDos = document.querySelector('#taskLists');
+
+//adds event listeners for the task area and contains functions for buttons on status change. 
+toDos.addEventListener('click', (event) => {
+  if(event.target.classList.contains('done')){
+    
+  const parentTask = event.target.parentElement.parentElement.parentElement;
+  
+  const taskId = Number(parentTask.dataset.taskId);
+
+  const task = taskManager.getTaskById(taskId);
+
+  //document.getElementById('task').getElementsByClassName('.sign').style.backgroundColor = '#78D5D7';
+
+  task.status = 'Completed';
+
+  //taskManager.save();
+  
+  taskManager.render();
+  
+  } else if (event.target.classList.contains('start')){
+      const parentTask = event.target.parentElement.parentElement.parentElement;
+  
+  const taskId = Number(parentTask.dataset.taskId);
+
+  const task = taskManager.getTaskById(taskId);
+
+  //document.getElementById('task').getElementsByClassName('.sign').style.backgroundColor = '#78D5D7';
+
+  task.status = 'In Progress';
+
+  //taskManager.save();
+  
+  taskManager.render();
+  }
+});
 
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,5 +1,7 @@
-const createTaskHtml = (name, description, assignedTo, dueDate, status) => `
-  <div class="col-lg-6 gx-3">
+// this is the HTML code to populate task cards with new task form feild inputs 
+
+  const createTaskHtml2 = (id, name, description, assignedTo, dueDate, status) => { 
+    let cardHtml = `<div class="col-lg-6 gx-3" data-task-id=${id}>
             <div class="card">
             <div class="card-header">
               <h3 class="card-title">${name}</h3>
@@ -26,9 +28,18 @@ const createTaskHtml = (name, description, assignedTo, dueDate, status) => `
                 <div class="col">
                   <div class="row">
                   <h4 class="card-subtitle col">Status:</h4>
-                  <div class="card-text col ">
-                  <p class="sign">${status}</p>
-                  </div>
+                  <div class="card-text col ">`;
+                  
+                  if(status == "Assigned"){
+                    cardHtml += `<p class="sign completed">${status}</p>`;
+                  }
+                  else if(status == "Completed"){
+                    cardHtml += `<p class="sign1">${status}</p>`;
+                  } else if(status == "In Progress"){
+                    cardHtml += `<p class="sign2">${status}</p>`;
+                  };
+                  
+                  cardHtml += `</div>
                 </div>
                   </div>
               </div>
@@ -36,52 +47,122 @@ const createTaskHtml = (name, description, assignedTo, dueDate, status) => `
               <div class="btn-group">
                 <button class="start">Start</button>
                 <button class="done">Completed</button>
-                <button class="arch">Archive</button>
+                <button class="remove">Remove</button>
               </div>
             
             </div>
-          </div>
-  `
-;
+          </div>`;
 
+          return cardHtml;
+  };
+
+// this is the task manger function
 
 class TaskManager {
+  // this is the code for the task IDs and task arrays
   constructor(currentId = 0) {
     this.tasks = [];
     this.currentId = currentId;
-      };
-
-  addTask(name, description, assignedTo, dueDate, status) {
-        const task = {
-        id: this.currentId++,
-        name: name,
-        description: description,
-        assignedTo: assignedTo,
-        dueDate: dueDate,
-        status: 'Assigned'
-         };
-         this.tasks.push(task);
   };
 
+
+  // this is a method that creates the array to add new tasks based on form values and pushes them to the array 
+  addTask(name, description, assignedTo, dueDate, status) {
+    const task = {
+      id: this.currentId++,
+      name: name,
+      description: description,
+      assignedTo: assignedTo,
+      dueDate: dueDate,
+      status: 'Assigned'
+    };
+    this.tasks.push(task);
+  };
+
+   // step 7 get task id
+   getTaskById(taskId) {
+     let foundTask;
+     for(let i = 0; i < this.tasks.length; i++) {
+       
+       const task = this.tasks[i];
+
+       if(task.id === taskId){
+         foundTask = task;
+       }
+     }
+     return foundTask;
+   }
+
+  // this renders the cards on the page
+
   render() {
+    // an array where the HTML is input for all the cards with the information collected on the form. 
     const tasksHtmlList = [];
 
-    for(let i = 0; i < this.tasks.length; i++){
+    // this loops through the tasks collected by add tasks and collects the data to put it in the HTML format. 
+
+    for (let i = 0; i < this.tasks.length; i++) {
+
+      // declares the varaible to work with based on the task being worked on in the array. 
       const taskNum = this.tasks[i];
 
+      //formats the date from the array nicely or the card. 
       const date = new Date(taskNum.dueDate);
       const formattedDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
-      
-      const taskHtml = createTaskHtml(taskNum.name, taskNum.description, taskNum.assignedTo, formattedDate, taskNum.status);
+
+      // this takes the current task in the array and gathers the information and variables for the form. 
+      const taskHtml = createTaskHtml2(taskNum.id,taskNum.name, taskNum.description, taskNum.assignedTo, formattedDate, taskNum.status);
+
+      // this pushes the card collected to the tasks arry. 
 
       tasksHtmlList.push(taskHtml);
     }
+    //this joins the array and puts new lines between each html code of the array. 
     const tasksHtml = tasksHtmlList.join('\n');
 
+    //this declares and selects the HTML 
     const taskLists = document.querySelector('#taskLists');
-    
+    // this changes the HTML 
     taskLists.innerHTML = tasksHtml;
   };
+
+ 
+  
+
+  //task 8 create a save method
+  /*save() {
+    const tasksJson = JSON.stringify(this.tasks);
+    localStorage.setItem('tasks', tasksJson);
+    const currentId = String(this.currentId);
+    localStorage.setItem('currentId', currentId);
+  };
+
+  //task 8 create a load method
+  load() {
+    if (localStorage.getItem('tasks')) {
+      const tasksJson = localStorage.getItem('tasks');
+      this.tasks = JSON.parse(tasksJson);
+    }
+
+    if (localStorage.getItem('currentId')) {
+      const currentId = localStorage.getItem('currentId')
+      this.currentId = Number(currentId);
+    }
+  }*/
+
+  /*task 9 create a deleteTask method
+  deleteTask(taskId) {
+    let newTasks = [];
+    for (let i = 0; i++) {
+      const task = this.tasks[i];
+      if (task.id !== taskId) {
+        newTasks.push(task);
+      }
+    }
+
+    this.tasks = newTasks;
+  }
+  */
 }
 
 //export {TaskManager}; 
